098ac45d26d43c842aaaf70fa882ac4adc52eb34
Seekertimo <seekertimo@gmail.com>
2019-10-19 18:12:19 +0200

    Add files via upload

--- shop/webshop.php
+++ shop/webshop.php
@@ -1,18 +1,29 @@
 <?php
+
+
 $item = array(
-    array ('Film 1','Dit is de eerste film','100','1'),
-    array ('Film 2','Dit is de tweede film','200','3'),
-    array ('Film 3','Dit is de derde film','300','6')
+    array ('Bioscoopbon 1','Dit is de eerste bioscoopbon','500','1'),
+    array ('Bioscoopbon 2','Dit is de tweede bioscoopbon','1000','3'),
+    array ('Bioscoopbon 3','Dit is de derde bioscoopbon','1500','6')
 );
 
 echo '<h1>Webshop</h1>';
 
-if (isset($_GET['actie']) && (isset($_GET['id']) && $_GET['id'] != '')){
-    echo 'Het id is '.$_GET['id'];
+if ((isset($_GET['actie']) && ($_GET['actie'] == 'bestel')) && (isset($_GET['id']) && ($_GET['id'] != ''))){
+    if (isset($_SESSION['bestel'])){
+        $bestelItem = explode('[@]', $_SESSION['bestel']);
+        if (!in_array($_GET['id'], $bestelItem)){
+            $_SESSION['bestel'] = $_GET['id'].'[@]'.$_SESSION['bestel'];
+        }
+
+        echo $_SESSION['bestel'];
+    }else{
+        $_SESSION['bestel'] = $_GET['id'].'[@]';
+    }
 }
 
 foreach($item AS $itemInfo){
     echo '<p><strong>'.$itemInfo[0].'</strong><br />'.$itemInfo[1].'</p>';
     echo '<p>&euro; '.($itemInfo[2]/100).',- </p>';
-    echo '<a href="?actie=bestel&id='.$itemInfo[3].'">Bestellen</a>';
+    echo '<a href="?module=webshop&actie=bestel&id='.$itemInfo[3].'">Bestellen</a>';
 }
